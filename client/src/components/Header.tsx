d88e770f47e572eb9f2591978d16df204a6f7abe
difwahab <difwahab@msn.com>
2025-05-19 23:37:02 -0400

    Update Header.tsx

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Link, useLocation } from "wouter";
 import { Menu, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import LangSwitcher from "@/components/ui/LangSwitcher";
 
 export default function Header() {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
